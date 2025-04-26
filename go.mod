7db92cf451fe9b50697f5911cd4768cb89f41035
s19835 <s19835@sci.pdn.ac.lk>
2025-04-26 14:48:31 +0530

    add register

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.24.1
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	golang.org/x/crypto v0.37.0
 )
--- handlers/auth.go
+++ handlers/auth.go
@@ -5,14 +5,35 @@ import (
 	"net/http"
 
 	"github.com/s19835/auth-app-go/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Register(w http.ResponseWriter, r *http.Request) {
 	var user models.User
 
+	// decode the json file
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
 		http.Error(w, "invalid request", http.StatusBadRequest)
 		return
 	}
+
+	// ensure already existing account
+	if _, exists := models.Users[user.Username]; exists {
+		http.Error(w, "user already exist", http.StatusBadRequest)
+		return
+	}
+
+	// hash the password
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Error hashing password", http.StatusInternalServerError)
+		return
+	}
+
+	// save the user
+	models.Users[user.Username] = string(hashPassword)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("User register successfully✨"))
 }
